3a910e89cad7414f77a4efc6c3baa2d1850b6caa
NwwVall <vidthekingrblx@gmail.com>
2025-02-12 16:51:10 +0700

    add : ProjectGaji-ReloadPush

--- Project4/end.php
+++ Project4/end.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>hasil gaji</title>
+    <title>hasil perhitungan gaji</title>
     
 </head>
 <body>
